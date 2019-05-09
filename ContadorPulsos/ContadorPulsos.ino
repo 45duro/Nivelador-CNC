49ca3d2f3e3ceef4cf0360d2d822bd83bf9dd683
Jonathan Gonzalez <jonathangc.awt@gmail.com>
2019-05-09 11:59:46 -0500

    Ya se soluciono el tema de las revoluciones, se concluye que la relacion del motor que estoy usando es de 131:1 y los conteos por flanco descendente son 16 para un total de 2096 tics en una revolucion ;D

--- ContadorPulsos/ContadorPulsos.ino
+++ ContadorPulsos/ContadorPulsos.ino
@@ -1,40 +1,49 @@
-int encoder = 3, encoderPos=0, encoderLast = 0, encoderActual = 0;
+//Relacion del motor de 131 con conteo de falnco de 16 tics por vuelta del motor
+// lo que quiere decir que para 360 grados de giro o una revolucion se obtendrá
+// 131*16 = 2096 tics a la salida
+
+int encoderPos=0, encoderLast = 0, encoderActual = 0;
 int radio = 36;
+int vueltas = 2;
 
 void setup() {
-  pinMode(encoder, INPUT);
-  Serial.begin(115200);
+  //Con este baudiaje se ajusta perfeccto
+  Serial.begin(57600);
 
+  //Si o si toca con interrupcion ya que de otromodo lo hace mal, 
+  //se queda o puierde pulso y el baudiaje de la comunicación lo vuelve loco
+  attachInterrupt(digitalPinToInterrupt(2), Encoder,FALLING);
+
+  //Declaro los pines de salida
   for(int j = 4; j <= 9; j++){
     pinMode(j,1);
   }
 
+  //Mandamos todos los pines a 0
   for(int j = 4; j <= 9; j++){
     digitalWrite(j,0);
   }
 
+  //Arranque de prueba para el motor
   digitalWrite(7,1);  //Horario
   digitalWrite(8,0); //Antiorario
   analogWrite(5, 20); //Pwm
 
 }
 
+//Funcion para contar
+void Encoder(){
+  encoderPos++;
+  Serial.println(encoderPos);
+}
+
 void loop() {
-  encoderActual=digitalRead(encoder);
 
-  if((encoderLast == 0) && (encoderActual == 1)){
-    encoderPos++;
-    Serial.print("Encoder POS: "); Serial.println(encoderPos);
-    //Serial.print("Enc pos rad: "); Serial.println(encoderPos*36);
-    //Serial.print("Enc pos real: "); Serial.println(encoderPos*0.2*3.141592*36);
-  }
-
-  encoderLast = encoderActual;
-
-  if(encoderPos >= 378){
-    digitalWrite(7,0);  //Horario
-    digitalWrite(8,0); //Antiorario
-    analogWrite(5, 0); //Pwm
+  //Determinamos la revolucion que quiero
+  if(encoderPos >= (2096*vueltas)){
+    digitalWrite(7,1);  //Horario
+    digitalWrite(8,1); //Antiorario
+    analogWrite(5, 20); //Pwm
     while(1);
 
   }
