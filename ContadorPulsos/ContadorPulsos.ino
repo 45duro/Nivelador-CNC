a4cd2e31ab266d177973d491d847c879455ced3f
Jonathan Gonzalez <jonathangc.awt@gmail.com>
2019-05-09 17:19:44 -0500

    Se puede usar la interrupcion de home perfectamente, cero inconvenientes y esto pinta bueno tambien se realiza la parte de funciones mucho mas limpio el codigo

--- ContadorPulsos/ContadorPulsos.ino
+++ ContadorPulsos/ContadorPulsos.ino
@@ -2,10 +2,12 @@
 // lo que quiere decir que para 360 grados de giro o una revolucion se obtendrá
 // 131*16 = 2096 tics a la salida
 
-int encoderPos=0, encoderLast = 0, encoderActual = 0;
-int radio = 36;
+unsigned long encoderPos=0;
 float vueltas = 10;
 int potencia = 255;
+//Motor
+byte giroAdelante = 12, giroAtras = 13, pinVelocidad = 11;
+
 
 void setup() {
   //Con este baudiaje se ajusta perfeccto
@@ -14,6 +16,7 @@ void setup() {
   //Si o si toca con interrupcion ya que de otromodo lo hace mal, 
   //se queda o puierde pulso y el baudiaje de la comunicación lo vuelve loco
   attachInterrupt(digitalPinToInterrupt(2), Encoder,FALLING);
+  attachInterrupt(digitalPinToInterrupt(3), inHome,RISING);
 
   //Declaro los pines de salida
   for(int j = 4; j <= 9; j++){
@@ -26,9 +29,9 @@ void setup() {
   }
 
   //Arranque de prueba para el motor
-  digitalWrite(7,1);  //Horario
-  digitalWrite(8,0); //Antiorario
-  analogWrite(5, potencia); //Pwm
+  digitalWrite(giroAdelante,1);  //Horario
+  digitalWrite(giroAtras,0); //Antiorario
+  analogWrite(pinVelocidad, potencia); //Pwm
 
 }
 
@@ -39,13 +42,32 @@ void Encoder(){
   //Serial.println(encoderPos);
 }
 
+void inHome(){
+
+  FrenarMotor();
+  delay(500);
+  pararMotor();
+  encoderPos=0;
+}
+
+void pararMotor(){
+  digitalWrite(giroAdelante,0);  //Horario
+  digitalWrite(giroAtras,0); //Antiorario
+  analogWrite(pinVelocidad, 0); //Pwm
+}
+
+void FrenarMotor(){
+  digitalWrite(giroAdelante,1);  //Horario
+  digitalWrite(giroAtras,1); //Antiorario
+  analogWrite(pinVelocidad, 255); //Pwm
+}
+
+
 void loop() {
 
   //Determinamos la revolucion que quiero
   if(encoderPos >= (2096*vueltas)){
-    digitalWrite(7,1);  //Horario
-    digitalWrite(8,1); //Antiorario
-    analogWrite(5, potencia); //Pwm
+    FrenarMotor();
     Serial.println(encoderPos);
     while(1);
 
